f58e8bebb2c8ba9fd8e540c1e418b79cc51e5936
ibson <ibsongomes8@gmail.com>
2025-12-04 16:00:27 -0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 DB_CONFIG = {
     "host": "localhost",
-    "user": "root",
-    "password": "Batatata0_",
+    "user": "root", #Aqui você deve colocar seu usuario do seu Mysql Workbench, normalmente é root.
+    "password": "sua_senha", #Aqui você deve colocar a senha do seu Mysql Workbench
     "database": "empresa",
+
 }
